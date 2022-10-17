b2ddff7227329eb237cd088b2b6a8abeed66829a
Yana Ponomarenko <marvelous1creature@gmail.com>
2022-10-17 22:43:55 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new Scaffold(
+          appBar: new AppBar(title: new Text('Yana Ponomarenko')),
+          body: new Text('Hello World!'))));
+}
